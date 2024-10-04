21204cd8a3df46ec18200272792936e1a15253f9
Pat Laplante <laplante.patrick@gmail.com>
2024-10-03 21:50:03 -0400

    first commit

---
+++ go.mod
@@ -0,0 +1,12 @@
+module example/splistg
+
+go 1.21.0
+
+require (
+	github.com/creack/goselect v0.1.2 // indirect
+	go.bug.st/serial v1.6.2 // indirect
+	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
+	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
+	rsc.io/quote v1.5.2 // indirect
+	rsc.io/sampler v1.3.0 // indirect
+)
---
+++ splistg.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	// "go.bug.st/serial"
+	"os"
+
+	"go.bug.st/serial/enumerator"
+)
+
+func main() {
+	args := os.Args[1:]
+	if len(args) == 0 {
+		listPorts()
+	} else {
+		findPort(args[0])
+	}
+}
+
+func listPorts() {
+	ports, err := enumerator.GetDetailedPortsList()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(ports) == 0 {
+		return
+	}
+	for _, port := range ports {
+		fmt.Printf("Port: %s\n", port.Name)
+		if port.Product != "" {
+			fmt.Printf("   Product Name: %s\n", port.Product)
+		}
+		if port.IsUSB {
+			fmt.Printf("   USB ID      : %s:%s\n", port.VID, port.PID)
+			fmt.Printf("   USB serial  : %s\n", port.SerialNumber)
+		}
+	}
+}
+
+func findPort(serialNumber string) {
+	ports, err := enumerator.GetDetailedPortsList()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, port := range ports {
+		if port.SerialNumber == serialNumber {
+			fmt.Printf("%s", port.Name)
+			return
+		}
+	}
+//	fmt.Printf("Port with serial number %s not found\n", serialNumber)
+}
